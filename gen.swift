d85d268dbe5d2424a846623239e22688314136b7
monjara <yaginumee@gmail.com>
2023-12-06 21:33:09 +0900

    add generator

---
+++ gen.swift
@@ -0,0 +1,37 @@
+#!/usr/bin/env swift
+
+import Foundation
+
+private func shell(_ command: String) {
+    let task = Process()
+    let pipe = Pipe()
+    
+    task.standardOutput = pipe
+    task.standardError = pipe
+    task.arguments = ["-c", command]
+    task.launchPath = "/bin/zsh"
+    task.standardInput = nil
+    task.launch()
+    task.waitUntilExit()
+    
+    let data = pipe.fileHandleForReading.readDataToEndOfFile()
+    let output = String(data: data, encoding: .utf8)!
+    
+    print("\(output)\n")
+}
+
+let fm = FileManager.default
+let currentDir = fm.currentDirectoryPath
+do {
+    let dirs = try fm.contentsOfDirectory(atPath: currentDir)
+
+    for d in dirs where fm.fileExists(atPath: "\(currentDir)/\(d)/Package.swift") {
+        shell("""
+            cd \(currentDir)/\(d) && \
+            swift build -c release && \
+            cp .build/release/\(d) ../bin/
+        """)
+    }
+} catch {
+
+}
