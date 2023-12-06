5e806ebdc38baf0261336bba21bac14622f68243
monjara <yaginumee@gmail.com>
2023-12-06 23:25:14 +0900

    fix

--- gen.swift
+++ gen.swift
@@ -2,10 +2,37 @@
 
 import Foundation
 
+struct ArgumentsParser {
+    var clearOnly: Bool = false
+
+    init(_ args: [String]) {
+        for a in args {
+            switch a {
+            case "--clear", "-c":
+                clearOnly = true
+            default:
+                break
+            }
+        }
+    }
+}
+
+private func clear(fm: FileManager) throws {
+    let binDir = "\(fm.currentDirectoryPath)/bin"
+    do {
+        let files = try fm.contentsOfDirectory(atPath: binDir)
+        for f in files where f != ".keep" {
+            try fm.removeItem(atPath: "\(binDir)/\(f)")
+        }
+    } catch {
+        throw error
+    }
+}
+
 private func shell(_ command: String) {
     let task = Process()
     let pipe = Pipe()
-    
+
     task.standardOutput = pipe
     task.standardError = pipe
     task.arguments = ["-c", command]
@@ -13,15 +40,26 @@ private func shell(_ command: String) {
     task.standardInput = nil
     task.launch()
     task.waitUntilExit()
-    
+
     let data = pipe.fileHandleForReading.readDataToEndOfFile()
     let output = String(data: data, encoding: .utf8)!
-    
+
     print("\(output)\n")
 }
 
-private func main() {
+private func main(_ args: ArgumentsParser) {
     let fm = FileManager.default
+
+    do {
+        try clear(fm: fm)
+        if args.clearOnly {
+            return
+        }
+    } catch {
+        print(error)
+        return
+    }
+
     let currentDir = fm.currentDirectoryPath
     do {
         let dirs = try fm.contentsOfDirectory(atPath: currentDir)
@@ -29,13 +67,17 @@ private func main() {
         for d in dirs where fm.fileExists(atPath: "\(currentDir)/\(d)/Package.swift") {
             shell("""
                 cd \(currentDir)/\(d) && \
-                swift build -c release && \
-                cp .build/release/\(d) ../bin/
+                swift build -c release
             """)
+
+            try fm.createSymbolicLink(
+                at: URL(fileURLWithPath: "\(currentDir)/bin/\(d)"),
+                withDestinationURL: URL(fileURLWithPath: "\(currentDir)/\(d)/.build/release/\(d)")
+            )
         }
     } catch {
-
+        print(error)
     }
 }
 
-main()
+main(ArgumentsParser(CommandLine.arguments))
