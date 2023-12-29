33d2667860fa151cfadc1b792a3f143b553a93d4
monjara <yaginumee@gmail.com>
2023-12-29 11:10:06 +0900

    add argument-parser

--- tttt/Package.swift
+++ tttt/Package.swift
@@ -5,10 +5,16 @@ import PackageDescription
 
 let package = Package(
     name: "tttt",
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.3.0"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
-            name: "tttt"),
+            name: "tttt",
+            dependencies: [
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+            ]),
     ]
 )
