081bc5642824eb6177b217aeeb7ad74f096fbeb8
monjara <yaginumee@gmail.com>
2024-02-18 13:31:17 +0900

    fix

--- applyXcode/Sources/ApplyXcode.swift
+++ applyXcode/Sources/ApplyXcode.swift
@@ -8,25 +8,6 @@ import ArgumentParser
 let XCODE_BUILD_SERVER = "xcode-build-server"
 let XCODE_BUILD_SERVER_REPOSITORY_URL = "https://github.com/SolaWing/xcode-build-server"
 
-private func hasDir(dir: String) -> Bool {
-    var isDir: ObjCBool = true
-    return FileManager.default.fileExists(atPath: "/tmp/\(dir)", isDirectory: &isDir)
-}
-
-private func retrieveXcodebuildServer() {
-    guard let repositoryUrl = URL(string: XCODE_BUILD_SERVER_REPOSITORY_URL) else {
-        print("failed to create url")
-        return
-    }
-
-    do {
-        try shellOut(to: .gitClone(url: repositoryUrl, to: "/tmp/xcode-build-server"))
-    } catch {
-        print("failed to retrieve xcode-build-server")
-        return
-    }
-}
-
 @main
 struct ApplyXcode: ParsableCommand {
     @Argument(help: "project name")
@@ -35,18 +16,12 @@ struct ApplyXcode: ParsableCommand {
     var isWorkSpace: Bool = false
 
     mutating func run() throws {
-
-        let hasXcodeBuildServer = hasDir(dir: XCODE_BUILD_SERVER)
-        if !hasXcodeBuildServer {
-            retrieveXcodebuildServer()
-        }
-
         let typeArgs = isWorkSpace ? "-workspace" : "-project"
         let ext = isWorkSpace ? "xcworkspace" : "xcodeproj"
 
         do {
             let output = try shellOut(
-                to: "/tmp/xcode-build-server/xcode-build-server",
+                to: XCODE_BUILD_SERVER,
                 arguments: [
                     "config",
                     "-scheme",
